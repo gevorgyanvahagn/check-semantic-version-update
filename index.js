71956940e8142436bbf8f53abe5ed9121399c447
Vahagn Gevorgyan <gevorgyanvahagn@gmail.com>
2022-10-19 01:50:01 +0400

    change action.yml inputs

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+
+  const currentVersion = core.getInput('current-version');
+  console.log(`The current version of the app is ${currentVersion}!`);
+
+  const previousVersion = core.getInput('previous-version');
+  console.log(`The previous version of the app is ${previousVersion}!`);
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  
 } catch (error) {
   core.setFailed(error.message);
 }
