395fbab83cc1765860c1a140c30cde7fd2f74eb0
Vahagn Gevorgyan <gevorgyanvahagn@gmail.com>
2022-10-19 02:23:23 +0400

    add debbug log

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ try {
   console.log(`The previous version of the app is ${previousVersion}!`);
 
   const diff = semver.diff(currentVersion, previousVersion)
+
+  const incrementMajor = semver.inc('previousVersion', 'major')
+  const incrementMinor = semver.inc('previousVersion', 'minor')
+  const incrementPatch = semver.inc('previousVersion', 'patch')
+
+  console.log(`Incremented major ${incrementMajor}!`);
+  console.log(`Incremented minor ${incrementMinor}!`);
+  console.log(`Incremented patch ${incrementPatch}!`);
+
+
   console.log(`The diff is ${diff}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
