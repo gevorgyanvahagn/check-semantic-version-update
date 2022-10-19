ff12edb59b06be90066b7a8167d9de58f5bc7323
Vahagn Gevorgyan <gevorgyanvahagn@gmail.com>
2022-10-19 08:44:14 +0400

    reorder functions

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ try {
   const incrementMinor = semver.inc(previousVersion, 'minor')
   const incrementPatch = semver.inc(previousVersion, 'patch')
 
-  if (cleanCurrentVersion == incrementMajor) {
-    console.log(`Incremented major version, new version is ${incrementMajor}!`);
-  }
-
-  if (cleanCurrentVersion == incrementMinor) {
-    console.log(`Incremented minor version, new version is ${incrementMinor}!`);
-  }
-
-  if (cleanCurrentVersion == incrementPatch) {
-    console.log(`Incremented patch version, new version is ${incrementPatch}!`);
-  }
-
-  if (cleanCurrentVersion !== incrementMajor, cleanCurrentVersion !== incrementMinor, cleanCurrentVersion !== incrementPatch) {
+  if (cleanCurrentVersion == incrementMajor || cleanCurrentVersion !== incrementMinor || cleanCurrentVersion !== incrementPatch) {
+    if (cleanCurrentVersion == incrementMajor) {
+        console.log(`Incremented major version, new version is ${incrementMajor}!`);
+      }
+    
+      if (cleanCurrentVersion == incrementMinor) {
+        console.log(`Incremented minor version, new version is ${incrementMinor}!`);
+      }
+    
+      if (cleanCurrentVersion == incrementPatch) {
+        console.log(`Incremented patch version, new version is ${incrementPatch}!`);
+      }
+  } else {
     console.log(`*********************************************************************************`);
     console.log(`Invalid app version ${currentVersion}, please check https://semver.org for best practicies of updating the semantic version of  the app`);
     console.log(`You can delete tag ${currentVersion} with the following commands:`);
